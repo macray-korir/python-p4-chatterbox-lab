99df1e7c725922cc1e4189bf63a5afe710b9c322
macray <macraykorir254@gmail.com>
2023-10-09 18:23:47 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +13,35 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )  # Removed created_at from here
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
+    else:
+        messages = Message.query.order_by(Message.created_at).all()
+        return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    if request.method == 'PATCH':
+        data = request.get_json()
+        message.body = data.get('body', message.body)
+        db.session.commit()
+        return jsonify(message.to_dict())
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({"message": f"Message with id {id} deleted"}), 200
+    else:
+        return jsonify(message.to_dict())
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Message(db.Model, SerializerMixin):
+class Message(db.Model):
     __tablename__ = 'messages'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    body = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
+    
+    def to_dict(self):
+        """Convert the message object to a dictionary."""
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None
+        }
